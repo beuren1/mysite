81b302d53bd4b4a4d1b79e3051d6a71456b9d08b
Josué Beuren <josuebeurensilvaa@gmail.com>
2026-07-24 13:37:50 -0300

    Adiciona view Hello World

--- mysite/tests/models/test_post.py
+++ mysite/tests/models/test_post.py
@@ -1,13 +1,12 @@
 import pytest
 
-from blog.factories import PostFactory
-
-
-@pytest.fixture
-def post_published():
-    return PostFactory(title='pytest with factory')
+from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_create_published_post(post_published):
-    assert post_published.title == 'pytest with factory'
+def test_post_view(client):
+    url = reverse('home')
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.content == b'Hello World'
